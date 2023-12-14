0002e8e92e4e2b6d13d939952084167bae079230
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-12-14 13:37:38 -0300

    add(keymap): Command to copy relative path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-local keymap = vim.keymap
+-- local keymap = vim.keymap
 
 -- BUffers
-keymap.set("n", "<leader>by", '<cmd>let @+ = expand("%")<cr>', { desc = "Copy relative path" })
+vim.keymap.set("n", "<leader>by", '<cmd>let @+ = expand("%:.")<cr>', { desc = "Copy relative path" })
