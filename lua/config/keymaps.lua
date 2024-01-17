671edb2410b5c39b0952f1c1b24701fb1a2c81b9
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2024-01-17 16:15:37 -0300

    add(keymaps)(neotree): add focus keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,12 @@ vim.keymap.set("n", "<leader>by", '<cmd>let @+ = expand("%:.")<cr>', { desc = "C
 
 --- Hop.nvim
 vim.keymap.set("", "f", "<cmd>HopWord<cr>", { desc = "Hint words (Hop.nvim)" })
+
+-- Neotree
+vim.keymap.set("n", "<leader>o", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd.wincmd("p")
+  else
+    vim.cmd.Neotree("focus")
+  end
+end, { desc = "Toggle Focus Neotree" })
