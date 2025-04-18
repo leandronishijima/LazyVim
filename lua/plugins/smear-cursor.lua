bace49575549ea6c6233a91f0dc1ad687dd9391f
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2025-04-18 13:29:48 -0300

    add(smear-cursour): new plugin (smear-cursor) 💅

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.5,
+      trailing_stiffness = 0.49,
+      distance_stop_animation = 0.5,
+      never_draw_over_target = false,
+      smear_between_buffers = false,
+      transparent_bg_fallback_color = "#303030",
+    },
+  },
+}
