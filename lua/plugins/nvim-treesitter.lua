045b686f627c6510c0399ea68fabc84555325daa
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2025-05-09 15:27:30 -0300

    add(treesitter): add incremental_selection

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -22,5 +22,14 @@ return {
       "ruby",
       "gleam",
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<cr>",
+        node_incremental = "<cr>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
   },
 }
