bf102ab27aa33c24d4e9dcd5f81d0fc3ae9285ca
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2025-10-11 15:33:41 -0300

    fix(treesitter)(endwise): remove config

--- lua/plugins/nvim-treesitter-endwise.lua
+++ lua/plugins/nvim-treesitter-endwise.lua
@@ -2,12 +2,5 @@ return {
   {
     "RRethy/nvim-treesitter-endwise",
     event = "VeryLazy",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        endwise = {
-          enable = true,
-        },
-      })
-    end,
   },
 }
