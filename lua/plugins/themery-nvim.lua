ff3450e2a8a639bd561a7b463d8701af16b931ff
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2026-04-28 16:46:21 -0300

    add(themery-nvim)(rose-pine): add themery + rose-pine

---
+++ lua/plugins/themery-nvim.lua
@@ -0,0 +1,24 @@
+return {
+  "zaldih/themery.nvim",
+  lazy = false,
+  config = function()
+    require("themery").setup({
+      themes = {
+        "kanagawa-wave",
+        "kanagawa-dragon",
+        "kanagawa-lotus",
+        "tokyonight-storm",
+        "tokyonight-moon",
+        "tokyonight-night",
+        "tokyonight-day",
+        "catppuccin-mocha",
+        "catppuccin-latte",
+        "catppuccin-frappe",
+        "catppuccin-macchiato",
+        "rose-pine-main",
+        "rose-pine-moon",
+        "rose-pine-dawn",
+      },
+    })
+  end,
+}
